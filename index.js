b9b4d598b7666486a6d3ef2141b02cf965bf6818
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-03-03 11:40:09 +0100

    add fav movies to index.js

--- index.js
+++ index.js
@@ -5,6 +5,65 @@ const app = express();
 
 app.use(morgan('common'));
 
+let favMovies = [
+  {
+    title: 'The Fifth Element',
+    releasedate: '1997',
+  },
+  {
+    title: 'Terminator',
+    releasedate: '1984',
+  },
+  {
+    title: 'The Deer Hunter',
+    releasedate: '1978',
+  },
+  {
+    title: 'Three Billboards Outside Ebbing, Missouri',
+    releasedate: '2017',
+  },
+  {
+    title: 'Metropolis',
+    releasedate: '1927',
+  },
+  {
+    title: 'Lawrence of Arabia',
+    releasedate: '1962',
+  },
+  {
+    title: 'Rear Window',
+    releasedate: '1954',
+  },
+  {
+    title: 'Interstellar',
+    releasedate: '2014',
+  },
+  {
+    title: 'The Silence of the Lambs',
+    releasedate: '1991',
+  },
+  {
+    title: 'One Flew Over the Cuckoo\'s Nest',
+    releasedate: '1991',
+  },
+  {
+    title: 'Fight Club',
+    releasedate: '1999',
+  },
+  {
+    title: 'Forrest Gumb',
+    releasedate: '1994',
+  },
+  {
+    title: 'Ghostbusters',
+    releasedate: '1984',
+  },
+  {
+    title: 'The Odd Couple',
+    releasedate: '1968',
+  },
+]
+
 // TODO
 
 app.listen(8080, () => {
