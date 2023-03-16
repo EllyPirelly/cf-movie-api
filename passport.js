ccbef86c89223e8337cdb3bd7ccd8db32be5fe4e
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-03-16 09:32:22 +0100

    introduce passport and jsonwebtoken, add basic passport.js

---
+++ passport.js
@@ -0,0 +1,58 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Models = require('./models.js');
+const passportJWT = require('passport-jwt');
+
+let Users = Models.User;
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+// basic HTTP authentication for login
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'userName',
+    passwordField: 'password'
+  },
+  (userName, password, callback) => {
+    console.log(userName + ' ' + password);
+
+    Users.fineOne(
+      {
+        userName: userName
+      },
+      (err, user) => {
+        if (err) {
+          console.log(err);
+          return callback(err);
+        }
+
+        if (!user) {
+          console.log('Incorrect username.');
+          return callback(null, false, {
+            message: 'Incorrect username or password.'
+          });
+        }
+
+        console.log('finished');
+        return callback(null, user);
+      }
+    );
+  }
+));
+
+// JWT authentication
+passport.use(new JWTStrategy(
+  {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+  },
+  (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+      .then((user) => {
+        return callback(null, user);
+      })
+      .catch((err) => {
+        return callback(err);
+      });
+  }
+));
