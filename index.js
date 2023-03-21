98c96813f077b8d3755073e0e155b3e1f413a886
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-03-21 15:09:07 +0100

    add env var for database connection

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/moviepooldb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.CONNECTION_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+// to connect locally
+// mongoose.connect('mongodb://localhost:27017/moviepooldb', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 const app = express();
 
