a56f4c5ccdd536737b7e2431c6491d36c8dac567
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-03-28 12:23:09 +0200

    delete return off of userSchema bcrypt

--- models.js
+++ models.js
@@ -49,7 +49,7 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
+  bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {
