310970e06fb426c8e8ad198987d631eb376477b3
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-03-28 16:20:12 +0200

    re-do promise in passport.js

--- passport.js
+++ passport.js
@@ -9,76 +9,64 @@ let Users = Models.User,
 
 // basic HTTP authentication for login
 // bugfix: tying the old way of password.use
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'userName',
-    passwordField: 'password',
-  },
-  (username, password, callback) => {
-    console.log(username + ' ' + password);
-    Users.findOne({ userName: username }, (error, user) => {
-      if (error) {
-        console.log(error);
-        return callback(error);
-      }
-
-      if (!user) {
-        console.log('Incorrect username.');
-        return callback(null, false, {
-          message: 'Incorrect username.',
-        });
-      }
-
-      if (!user.validatePassword(password)) {
-        console.log('Incorrect password.');
-        return callback(null, false, {
-          message: 'Incorrect password.',
-        });
-      }
-
-      console.log('finished');
-      return callback(null, user);
-    });
-  }
-));
-
-// with promises, commented out for the moment
 // passport.use(new LocalStrategy(
 //   {
 //     usernameField: 'userName',
-//     passwordField: 'password'
+//     passwordField: 'password',
 //   },
-//   (userName, password, callback) => {
-//     console.log(userName + ' ' + password);
-
-//     Users.findOne(
-//       {
-//         userName: userName
+//   (username, password, callback) => {
+//     console.log(username + ' ' + password);
+//     Users.findOne({ userName: username }, (error, user) => {
+//       if (error) {
+//         console.log(error);
+//         return callback(error);
 //       }
-//     ).then((user) => {
+
 //       if (!user) {
 //         console.log('Incorrect username.');
 //         return callback(null, false, {
-//           message: 'Incorrect username.'
+//           message: 'Incorrect username.',
 //         });
 //       }
-//     }).then((user) => {
+
 //       if (!user.validatePassword(password)) {
 //         console.log('Incorrect password.');
 //         return callback(null, false, {
-//           message: 'Incorrect password.'
+//           message: 'Incorrect password.',
 //         });
 //       }
 
 //       console.log('finished');
 //       return callback(null, user);
-
-//     }).catch((err) => {
-//       console.log(err);
-//       return callback(err);
 //     });
 //   }
 // ));
+
+// promises
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'userName',
+      passwordField: 'password',
+    },
+    (username, password, callback) => {
+      console.log(username + ' ' + password);
+      Users.findOne({ userName: username })
+        .then((user) => {
+          if (!user) {
+            console.log('Incorrect username or password.');
+            return callback(null, false, {
+              message: 'Incorrect username or password.'
+            });
+          }
+          console.log('finished');
+          return callback(null, user);
+        }).catch((err) => {
+          console.log(err);
+          return callback(err, false);
+        });
+    }
+  ));
 
 // JWT authentication
 passport.use(new JWTStrategy(
