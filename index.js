131784057b7f860dcc5003b13035955f2ffa897f
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-04-19 08:16:28 +0200

    allow all origins, fix netlify link

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // ALL origins have access (default)
 // this way is not recommended but only allowing certain ones does not work atm
-app.use(cors());
+// app.use(cors());
 
 // only CERTAIN origins have access - does not work
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://movie-pool-app.netlify.app/'];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       // if a specific origin isn't found on the list of allwed origins
-//       let message = 'The CORS policy for this app does NOT allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movie-pool-app.netlify.app'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // if a specific origin isn't found on the list of allwed origins
+      let message = 'The CORS policy for this app does NOT allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
